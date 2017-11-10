1297bf8a4edfd66f18e2996cd2585c72f7a75357
Александр Русак <a.rusak@corp.lenvendo.ru>
2017-11-10 16:07:14 +0300

    lession-2 done

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Comment.css";
 
 class Comment extends Component {
+  handleDelete = evt => {
+    
+  };
   render() {
-    return <div />;
+    const { text, onDelete } = this.props;
+    return (
+      <article>
+        <p>{text}</p>
+        <span
+          className="delete"
+          role="img"
+          aria-label="Delete"
+          onClick={() => onDelete(this.props.id)}
+        >
+          ❌
+        </span>
+      </article>
+    );
   }
 }
 
